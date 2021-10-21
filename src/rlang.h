d5a0e4d3beb050816e886993c17673e61560b373
DavisVaughan <davis@rstudio.com>
2021-10-21 16:09:57 -0400

    Try including Rinternals.h before rlang.h to give it C++ linkage

--- src/rlang.h
+++ src/rlang.h
@@ -1,6 +1,10 @@
 #ifndef RRAY_RLANG_H
 #define RRAY_RLANG_H
 
+// Include Rinternals.h with C++ linkage to avoid rlang including it while
+// having C linkage, which causes issues with the GHA Mac machine
+#include <Rinternals.h>
+
 extern "C" {
 #include "rlang/rlang.h"
 }
